9f5c70922b7e1ce28d32414e024226aff40a4547
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-02-10 19:12:22 +0100

    feat: add HeaderComponent and PostCreateComponent with Angular Material integration; update styles and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PostCreateComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-post-create',
-  imports: [],
+  imports: [MatInputModule, MatButtonModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.scss'
 })
 export class PostCreateComponent {
-
+  onAddPost() {
+    alert('Post added!')
+  }
 }

85d3f7b4092e28e4817ac58648cd8485a616c782
Mamadou Bah <42182390+mBah1705@users.noreply.github.com>
2025-04-20 18:42:24 +0200

    chore: app-root selector was gone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 
 @Component({
+  selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
